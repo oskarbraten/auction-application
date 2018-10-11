288faefb2b1e16f589c993ff5b12bd26836616cb
148913 <148913@stud.hvl.no>
2018-10-11 16:49:07 +0200

    Uppdated and implemeted getRating and getSellersProductRating in product. -1 is no rating
    Signed-off-by: 148913 <148913@stud.hvl.no>

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -54,19 +54,43 @@ public class Product {
 		int numbOfRatings = 0;
 
 		Iterator aIt = auctions.iterator();
-
-		while(aIt.hasNext()){
-			Auction a = (Auction) aIt.next();
-			Feedback f = a.getFeedback();
-			if(f != null || f.getProductRating() != null){ //Feedback exists and a rating exists
-				rating += f.getProductRating();
-				numbOfRatings ++;
+		if(aIt.hasNext()) {
+			while (aIt.hasNext()) {
+				Auction a = (Auction) aIt.next();
+				Feedback f = a.getFeedback();
+				if (f != null || f.getProductRating() != null) { //Feedback exists and a rating exists
+					rating += f.getProductRating();
+					numbOfRatings++;
+				}
 			}
+		}else {
+			return -1; //No rating exist
 		}
 
 		return (rating/numbOfRatings);
 	}
 
+	public double getSellersProductRating(){
+		double rating = 0;
+		int numbOfRatings = 0;
+
+		Iterator aIt = auctions.iterator();
+
+		if(aIt.hasNext()) {
+			while (aIt.hasNext()) {
+				Auction a = (Auction) aIt.next();
+				Feedback f = a.getFeedback();
+				if (f != null || f.getSellerRating() != null) { //Feedback exists and a rating exists
+					rating += f.getSellerRating();
+					numbOfRatings++;
+				}
+			}
+		} else {
+			return -1; //No rating exist
+		}
+
+		return (rating/numbOfRatings);
+	}
 
 
 	public Integer getId() {
